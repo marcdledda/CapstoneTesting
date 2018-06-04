71a5d9f7bbbc4fc5b36936f802796e487131612a
Marc Ledda <marcdl636@gmail.com>
2018-06-03 23:49:21 -0500

    initial files w/ magnet testing

---
+++ Assets/Scripts/MagnetAttract.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetAttract : MonoBehaviour {
+
+	private bool inside;
+	private float force = 1.5f;
+	private Transform player;
+	private Transform cube;
+	private Rigidbody cubeRB;
+
+	// Use this for initialization
+	void Start () {
+		inside = false;
+		player = GameObject.Find("PlayerCube").GetComponent<Transform>();
+		cube = GetComponentInParent<Transform>();
+		cubeRB = GetComponentInParent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			inside = true;
+			cubeRB.useGravity = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Player"){
+			inside = false;
+			cubeRB.useGravity = true;
+		}
+	}
+
+	void FixedUpdate(){
+		if (inside){
+			Vector3 magnetField = player.position - cube.position;
+			cubeRB.AddForce(force*magnetField);
+		}
+	}
+
+}
---
+++ Assets/Scripts/TestAddForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAddForce : MonoBehaviour {
+
+	// private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		// rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		// if(Input.GetKeyDown("space")){
+		// 	rb.AddForce(transform.forward*100);
+		// }
+	}
+}
