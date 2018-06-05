102d8aee4b16fd4af7ad4b65b53d9716139176d6
Marc Ledda <marcdl636@gmail.com>
2018-06-04 20:21:21 -0500

    created script which sizes platform based on play area

---
+++ Assets/Scripts/DodgeWallState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeWallState : MonoBehaviour {
+
+	private Vector3 getWidth;
+	[SerializeField]
+	private Transform platform;
+
+	// Use this for initialization
+	void Start () {
+		getWidth = OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea);
+		platform.localScale = new Vector3(getWidth.x, 1f, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
