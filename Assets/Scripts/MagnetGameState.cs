fa3a5672cdf6587125e3637cc3653858b4f26d18
marcdledda <marcdl636@gmail.com>
2018-06-05 15:36:56 -0500

    added test load screen

---
+++ Assets/Scripts/MagnetGameState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MagnetGameState : MonoBehaviour {
+
+	private bool loading = false;
+
+	// Use this for initialization
+	void Start () {
+		// SceneManager.LoadScene("NotVRLoad", LoadSceneMode.Additive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space) && !loading){
+			loading = true;
+			// SceneManager.GetSceneByName("NotVRLoad");
+			SceneManager.LoadScene("NotVRLoad");
+		}
+	}
+}
