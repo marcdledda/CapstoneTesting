307f89aa366c5cea91159585609c8637fa2bff0f
Marc Ledda <marcdl636@gmail.com>
2018-06-04 23:18:18 -0500

    added random spawner of wall based on size of platform

--- Assets/Scripts/DodgeWallState.cs
+++ Assets/Scripts/DodgeWallState.cs
@@ -7,15 +7,46 @@ public class DodgeWallState : MonoBehaviour {
 	private Vector3 getWidth;
 	[SerializeField]
 	private Transform platform;
+	[SerializeField]
+	private GameObject cubePrefab;
+	[SerializeField]
+	private int dodgeAmount;
+	private bool changePlat = false;
+	private bool spawnNext = true;
+	private bool cubeExist = false;
 
 	// Use this for initialization
 	void Start () {
 		getWidth = OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea);
-		platform.localScale = new Vector3(getWidth.x/10f, 1f, 3f);
+		if (getWidth.x < 2.1f){
+			platform.localScale = new Vector3(getWidth.x/10f, 1f, 1.5f);
+			changePlat = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (spawnNext && !cubeExist){
+			if (changePlat){
+				float range = (getWidth.x/10f) * 5f - 0.25f;
+				float positionX = Random.Range(-range, range);
+
+				Vector3 objectPosition = new Vector3(positionX, 1.5f, 7f);
+
+				Instantiate(cubePrefab, objectPosition, Quaternion.identity);
+
+				spawnNext = false;
+				cubeExist = true;
+			} else {
+				float positionX = Random.Range(-0.75f, 0.75f);
+
+				Vector3 objectPosition = new Vector3(positionX, 1.5f, 7f);
+
+				Instantiate(cubePrefab, objectPosition, Quaternion.identity);
+
+				spawnNext = false;
+				cubeExist = true;
+			}
+		}
 	}
 }
