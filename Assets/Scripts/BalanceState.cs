9178789ce8d2aa418bc30c972470b147f016b2f5
Marc Ledda <marcdl636@gmail.com>
2018-06-06 02:24:58 -0500

    attempting balancing script

--- Assets/Scripts/BalanceState.cs
+++ Assets/Scripts/BalanceState.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BalanceState : MonoBehaviour {
 
 	private Vector3 getSize;
 	[SerializeField]
 	private Transform platform;
+	[SerializeField]
+	private Text gameStateText;
+	[SerializeField]
+	private GameObject blockPrefab;
+	private float sizeZ = 0f;
+	private int blockAmount;
 
 	// Use this for initialization
 	void Start () {
 		getSize = OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea);
-		platform.localScale = new Vector3(getSize.z, 3f, getSize.z);
+		getSize.z = sizeZ;
+		platform.localScale = new Vector3(getSize.z + 0.15f, 3f, getSize.z + 0.15f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		bool pressA = OVRInput.GetUp(OVRInput.Button.One);
+		if (pressA){
+			SpawnBlock();
+			gameStateText.text = "Catch and Balance the Cubes!";
+		}
 	}
+
+	public void SpawnBlock(){
+		if (blockAmount > 10){
+			float range = sizeZ * 0.5f - 0.06f;
+			float position = Random.Range(-range, range);
+
+			Vector3 objectPosition = new Vector3(position, 3f, range);
+
+			Instantiate(blockPrefab, objectPosition, Quaternion.identity);
+			blockAmount++;
+		}
+	}
+
 }
---
+++ Assets/Scripts/blockMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockMovement : MonoBehaviour {
+
+	public static float speed;
+	[SerializeField]
+	private Rigidbody blockPrefab;
+	public BalanceState stateScript;
+	private bool collisionOn = false;
+
+	// Use this for initialization
+	void Start () {
+		GameObject stateObject = GameObject.Find("BalanceState");
+		BalanceState stateScript = (BalanceState) stateObject.GetComponent(typeof(BalanceState));
+		speed = 1f;
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Platform" || other.gameObject.tag == "buildBlock") {
+			collisionOn = true;
+			stateScript.SpawnBlock();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!collisionOn){
+			blockPrefab.AddForce(speed * -transform.up);
+		} else if (collisionOn) {
+			blockPrefab.useGravity = true;
+		}
+		
+	}
+}
